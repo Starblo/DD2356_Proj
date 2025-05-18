c5465180d0dc566859ad1d54cee2d79b10f6f079
Zixiang Hong <zixhon@login1.hsn.dardel.pdc.kth.se>
2025-05-19 00:06:00 +0200

    add plot

--- Assignment-IV/exercise4/job.sh
+++ Assignment-IV/exercise4/job.sh
@@ -5,7 +5,7 @@
 #SBATCH -A edu25.DD2356
 
 # The name of the script is myjob
-#SBATCH -J E4Q4opt80000
+#SBATCH -J E4Q4plot
 
 # The partition
 #SBATCH -p shared
@@ -55,13 +55,33 @@ module load PDC/23.12
 module load openblas/0.3.28-cpeGNU-23.12
 module load score-p/8.4-cpeGNU
 
-mpicc -o blas_mvm blas_example.c -lopenblas -lm
-srun --ntasks=1 ./blas_mvm
+# mpicc -o blas_mvm blas_example.c -lopenblas -lm
+# srun --ntasks=1 ./blas_mvm
 
-scorep mpicc -o blas_para_opt blas_para_optimized.c -lopenblas -lm
-export SCOREP_ENABLE_PROFILING=true
-export SCOREP_ENABLE_TRACING=false
-srun -n 8 ./blas_para_opt
-export SCOREP_ENABLE_PROFILING=false
-export SCOREP_ENABLE_TRACING=true
-srun -n 8 ./blas_para_opt
+# scorep mpicc -o blas_para_opt blas_para_optimized.c -lopenblas -lm
+# export SCOREP_ENABLE_PROFILING=true
+# export SCOREP_ENABLE_TRACING=false
+# srun -n 8 ./blas_para_opt
+# export SCOREP_ENABLE_PROFILING=false
+# export SCOREP_ENABLE_TRACING=true
+# srun -n 8 ./blas_para_opt
+
+# Loop over different matrix sizes
+for matrixsize in 1000 10000 20000 40000
+do
+    # Compile and run the serial version (blas_mvm)
+    echo "Running serial version with matrix size $matrixsize"
+    mpicc -o blas_mvm blas_example.c -lopenblas -lm
+    srun --ntasks=1 ./blas_mvm $matrixsize
+
+    echo "----------------------------------------"
+done
+for matrixsize in 1000 10000 20000 40000 50000 60000 70000 80000
+do
+    # Compile and run the parallel version (blas_para)
+    echo "Running parallel version with matrix size $matrixsize"
+    mpicc -o blas_para_opt blas_para_optimized.c -lopenblas -lm
+    srun -n 16 ./blas_para_opt $matrixsize
+
+    echo "----------------------------------------"
+done
